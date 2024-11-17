3380b99c901f3ef197d284e0d61bd09f884369bc
Debraj Kundu <debrajkundu657@gmail.com>
2024-11-17 11:03:59 +0530

    Add some default value in schema

--- app/models/InterviewModel.js
+++ app/models/InterviewModel.js
@@ -1,12 +1,21 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    mockid : String,
+    mockid : {
+        type : String,
+        unique : true
+    },
     jobTitle : String,
     jobDesc : String,
     jobExperience : String,
-    createdBy : String,
-    createdAt : String,
+    createdBy : {
+        type : String,
+        default : "Unknown"
+    },
+    createdAt : {
+        type : String,
+        default : Date.now.toString()
+    },
     jsonResponse : String
 })
 
