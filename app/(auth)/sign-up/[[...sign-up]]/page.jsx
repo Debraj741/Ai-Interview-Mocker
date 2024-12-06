16bfd235761b1a5bba5b060d5efba89579810dcd
Debraj Kundu <debrajkundu657@gmail.com>
2024-12-06 19:42:12 +0530

    Signup change position

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,5 +1,9 @@
 import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
-  return <SignUp signInForceRedirectUrl={'/dashboard'}/>
+  return(
+    <div className='flex items-center justify-center'>
+      <SignUp signInForceRedirectUrl={'/dashboard'}/>
+    </div>
+    ) 
 }
