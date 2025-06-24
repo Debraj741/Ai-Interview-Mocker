02fe0ef91f0de70022b484fb79c19a47c6d7a82b
Debraj Kundu <debrajkundu657@gmail.com>
2025-06-24 20:26:03 +0530

    New feature remove as it not completed yet

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -14,7 +14,7 @@ export default function Dashboard() {
       </div>
 
       {/* Features List */}
-      <FeatureAssistant/>
+      {/* <FeatureAssistant/> */}
 
       {/* Previous Interview List */}
 
